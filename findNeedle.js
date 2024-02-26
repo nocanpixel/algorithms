61c889ba12505730f78e723862cebda21ec76c11
Juan Camilo Carreno Beleno <nocan@Juans-MacBook-Air.local>
2024-02-26 11:18:16 +0100

    isAllPossibilities problem

---
+++ findNeedle.js
@@ -0,0 +1,8 @@
+function findNeedle(haystack) {
+    // your code here
+    for (let i = 0; i < haystack.length; i++) {
+       if(haystack[i]==='needle') {return `found the needle at position ${i}`}
+    }
+  }
+ 
+  console.log(findNeedle(['3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false]))
